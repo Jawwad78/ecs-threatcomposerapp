fe409355cb895a049b5fa3c1a0d798370b2e6328
jawwad78 <ja66730@gmail.com>
2025-12-14 08:21:14 +0000

    formatted files properly

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,200 +1,200 @@
 variable "cidr_block_vpc" {
- type = string
- description = "VPC cidr block"
+  type        = string
+  description = "VPC cidr block"
 }
 
 variable "cidr_block_public_1" {
- type = string
- description = "public subnet 1"
+  type        = string
+  description = "public subnet 1"
 }
 
 variable "cidr_block_public_2" {
- type = string
- description = "public subnet 2"
+  type        = string
+  description = "public subnet 2"
 }
 
 variable "cidr_block_private_1" {
- type = string
- description = "private subnet 1"
+  type        = string
+  description = "private subnet 1"
 }
 
 variable "cidr_block_private_2" {
- type = string
- description = "private subnet 2"
+  type        = string
+  description = "private subnet 2"
 }
 
 
 variable "availability_zone_1" {
-  type = string
+  type        = string
   description = "1st az location "
 }
 
 variable "availability_zone_2" {
-  type = string
+  type        = string
   description = "2nd az location "
 }
 
 variable "cpu" {
-  type = string
+  type        = string
   description = "cpu for container"
 }
 
 variable "memory" {
-  type = string
+  type        = string
   description = "memory for container"
 }
 
 variable "image_arn" {
-  type = string
+  type        = string
   description = "container image arn"
 }
 
 variable "operating_system_family" {
-  type = string
+  type        = string
   description = "operating system"
 }
 
 variable "cpu_architecture" {
-  type = string
+  type        = string
   description = "cpu architecture"
 }
 
 variable "retention_in_days" {
-  type = number
+  type        = number
   description = "how long to retain log events"
 }
 
 variable "log_group_class" {
-  type = string
+  type        = string
   description = "log class"
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "region"
 }
 
 variable "desired_count" {
-  type = number
+  type        = number
   description = "how many tasks we need"
 }
 
 variable "deployment_minimum_healthy_percent" {
-  type = number
+  type        = number
   description = "any new configs we do to task definition, our new task must start first before removing old one "
 }
 
 variable "deployment_maximum_percent" {
-  type = number
+  type        = number
   description = "how many new tasks we want if we change our task definition "
 }
 
 variable "assign_public_ip" {
-  type = string
+  type        = string
   description = "public ip to task eni, since my tasks are in private subnet,it will be false"
 }
 
 variable "port_80" {
-  type = number
+  type        = number
   description = "port for http that we use"
 }
 
 variable "port_443" {
-  type = number
+  type        = number
   description = "port for https that we use"
 }
 
 variable "ecs_sg_ports_1" {
-  type = number
+  type        = number
   description = "port inbound ecs to alb"
 }
 
 variable "tcp" {
-  type = string
+  type        = string
   description = "tcp"
 }
 
 
 variable "ecs_sg_ports" {
-  type = number
+  type        = number
   description = "ecs inbound and outbound ports"
 }
 
 
 variable "cidr_ipv4" {
-  type = string
+  type        = string
   description = "ipv4 cidr"
 }
 
 
 variable "internal" {
-  type = string
+  type        = string
   description = "is alb internal"
 }
 
 variable "load_balancer_type" {
-  type = string
+  type        = string
   description = "alb load balancer"
 }
 
 variable "alb_protocol" {
-  type = string
+  type        = string
   description = "http protocol for alb"
 }
 
 variable "alb_protocol_listener" {
-  type = string
+  type        = string
   description = "https protocol for alb"
 }
 
 variable "ssl_policy" {
-  type = string
+  type        = string
   description = "the ssl policy"
 }
 
 
 variable "target_type" {
-  type = string
+  type        = string
   description = "ip address target"
 }
 
 variable "container_name" {
-  type = string
+  type        = string
   description = "container name"
 }
 
 variable "container_port" {
-  type = number
+  type        = number
   description = "container port"
 }
 
 variable "hostPort" {
-  type = number
+  type        = number
   description = "host container port"
 }
 
 variable "domain_name" {
-  type = string
+  type        = string
   description = "my domain name "
 }
 
 variable "validation_method" {
-  type = string
+  type        = string
   description = "validation method "
 }
 
 variable "acm_tag" {
-  type = string
+  type        = string
   description = "my acm cert tag"
 }
 
 variable "key_algorithm" {
-  type = string
+  type        = string
   description = "acm key algorithm"
 }
 
 
 variable "health_check_path" {
-  type = string
+  type        = string
   description = "health check path"
 }
